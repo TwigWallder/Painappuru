413fd13d73c64b43e43809a4560ce952ee04a94f
TwigWallder <45337156+TwigWallder@users.noreply.github.com>
2021-09-23 21:30:23 +0200

    Update conf.lua

--- Painappuru/conf.lua
+++ Painappuru/conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
-    t.title = "Games"
-    t.version = "11.3" --La version de Lôve pour la qu'elle le jeu à été créer.
+    t.title = "Games"  --Modification du titre de la fenêtre
+    t.version = "11.3" --La version de Löve pour la qu'elle le jeu à été créer.
     t.console = true   --Activer la console ou pas (Utile pour le "mode dev")
     t.window.width = 1280
     t.window.height = 720
