ae24513f09d28d97ba4e2de2ca9b6e4c8d91ac01
TwigWallder <45337156+TwigWallder@users.noreply.github.com>
2021-09-23 21:28:52 +0200

    Add files via upload

---
+++ Painappuru/conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+    t.title = "Games"
+    t.version = "11.3" --La version de Lôve pour la qu'elle le jeu à été créer.
+    t.console = true   --Activer la console ou pas (Utile pour le "mode dev")
+    t.window.width = 1280
+    t.window.height = 720
+end
